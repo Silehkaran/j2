d312320efd52df0c1de387c8ec9bede33b6e1c91
mohammad mehdi silehkaran <mohammad.mehdi.silehkaran@gmail.com>
2018-08-30 23:42:03 +0430

    remove imports

--- src/Reshte/reshte.java
+++ src/Reshte/reshte.java
@@ -1,7 +1,6 @@
 package Reshte;
-import java.util.ArrayList;
+
 import java.util.HashMap;
-import java.util.List;
 import java.util.Scanner;
 public class reshte {
     public static void main(String arges[]) {
